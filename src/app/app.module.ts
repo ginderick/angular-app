658abdcc2d6cfa4b5198fcddb3da90fc3e0db60d
Gin Derick <ginderick@pop-os.localdomain>
2023-04-14 18:50:11 +0800

    add ProductAlertsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   imports: [
@@ -14,7 +15,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     ReactiveFormsModule,
     RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
   ],
-  declarations: [AppComponent, TopBarComponent, ProductListComponent],
+  declarations: [AppComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
